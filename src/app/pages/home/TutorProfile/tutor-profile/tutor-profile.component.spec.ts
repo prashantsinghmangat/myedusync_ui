f0d88ba2ef7dd5fe71e82d849efeba3111eeae8a
AmanK2500 <40556916+AmanK2500@users.noreply.github.com>
2024-09-17 20:52:20 +0530

    Tutor Dashboard / Login UI

---
+++ src/app/pages/home/TutorProfile/tutor-profile/tutor-profile.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TutorProfileComponent } from './tutor-profile.component';
+
+describe('AboutUsComponent', () => {
+  let component: TutorProfileComponent;
+  let fixture: ComponentFixture<TutorProfileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TutorProfileComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TutorProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
